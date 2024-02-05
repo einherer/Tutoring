a69f81bcba6c46c1f050548b99306b698da166a7
einherer <johanneshumann@hotmail.com>
2024-02-05 14:01:36 +0100

    preperation for debugging

--- 20240205/debugging.py
+++ 20240205/debugging.py
@@ -1,3 +1,7 @@
+'''
+original code with errors:
+
+
 class Person:
     def __init__(self, name, age, location):
         self.name = name
@@ -20,4 +24,26 @@ results = produce_people()
 
 for result in results:
     print(result)
-    print(result.__dict__)
+'''
+class Person:
+    def __init__(self, name, age, location):
+        self.name = name
+        self.age = age
+        self.location = location
+
+def produce_people():
+    from data import ages, names, locations
+
+    people_list = []
+
+    for n, a, l in name, age, location:
+        person_instance = Person(name=n, age=a, location=l)
+        people_list.append(person_instance)
+
+    return people_list
+
+
+results = produce_people()
+
+for result in results:
+    print(result)
